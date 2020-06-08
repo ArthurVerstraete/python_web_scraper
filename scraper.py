bd094d4a57f00ab6ca27e3e308548afcbd7324f8
Arthur Verstraete <verstraetearthur@gmail.com>
2020-06-08 17:52:14 +0200

    updated script to write to file and added some more columns to second table

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from prettytable import PrettyTable as pt
 import time
+import datetime
+now = datetime.datetime.now()
 
 my_url = 'https://tweakers.net/gallery/1204824/wenslijst/'
 
@@ -24,7 +26,7 @@ outputTable = pt()
 outputTable.field_names = ["Title", "Cheapest Shop", "Price #1", "Second Cheapest Shop", "Price #2", "Third Cheapest Shop", "Price #3"]
 
 outputTable2 = pt()
-outputTable2.field_names = ["#products", "Total"]
+outputTable2.field_names = ["#products", "Total", "Total According To Tweakers", "Difference", "Procentual Difference"]
 
 total = 0
 i = 0
@@ -32,6 +34,9 @@ i = 0
 for tableRow in tableRows:
     numberOfRows = len(tableRows)
     if (i == (numberOfRows - 1)):
+        priceAccordingToTweakers = tableRow.find("td", {"class":"price"}).text
+        priceAccordingToTweakers = priceAccordingToTweakers.replace('€', '').replace('.', '').replace(',', '.').replace('-', '0').replace(' ', '')
+        priceAccordingToTweakers = round(float(priceAccordingToTweakers), 2)
         break
     tableRowUrl = tableRow.find("p", {"class":"ellipsis"}).a["href"]
     print("Scraping " + tableRowUrl)
@@ -59,14 +64,14 @@ for tableRow in tableRows:
         cheapestShopsName[int(x/6)] = cheapestShop.find("td", {"class":"shop-name"}).p.a.text.replace('\n', '')
         cheapestShopsPrice[int(x/6)] = cheapestShop.find("td", {"class":"shop-price"}).p.a.text.replace('\n', '')
     outputTable.add_row([title, cheapestShopsName[0], cheapestShopsPrice[0], cheapestShopsName[1], cheapestShopsPrice[1], cheapestShopsName[2], cheapestShopsPrice[2]])
-    price = cheapestShopsPrice[0].replace(' ', '')
-    price = price.replace('€', '')
-    price = price.replace('-', '0')
-    price = price.replace(',', '.')
+    price = cheapestShopsPrice[0].replace(' ', '').replace('€', '').replace('-', '0').replace(',', '.')
     total = total + float(price)
     i = i + 1
     time.sleep(20)
 
 print(outputTable)
-outputTable2.add_row([len(tableRows)-1, round(total, 2)])
+outputTable2.add_row([len(tableRows)-1, "€ " + str(round(total, 2)), "€ " + str(priceAccordingToTweakers), "€ " + str(round(total-priceAccordingToTweakers, 2)), str(round((((total-priceAccordingToTweakers)/priceAccordingToTweakers)*100), 4)) + " %"])
 print(outputTable2)
+f = open(str(now.strftime("%d-%m-%Y_%H:%M:%S")).replace(':', '') + ".txt", 'a')
+f.write(str(outputTable))
+f.write("\n" + str(outputTable2))
